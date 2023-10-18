6ceda9377222aa31e6d06cc070e5c53acbe20ab4
LeonVolmary <leon.v2000@gmx.de>
2023-10-18 13:32:41 +0200

    Daten speicherung mit mqtt

--- app/src/main/java/com/praktikum/bomoapp/activities/MainActivity.kt
+++ app/src/main/java/com/praktikum/bomoapp/activities/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.praktikum.bomoapp.activities.MapActivity
+import com.praktikum.bomoapp.activities.SettingsActivity
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
