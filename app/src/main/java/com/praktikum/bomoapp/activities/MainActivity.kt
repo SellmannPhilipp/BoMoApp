21879c86fc7a0be56336149147b1d40debff322c
Philipp Sellmann <philippsellmann1@gmail.com>
2023-10-18 15:15:52 +0200

    Code refactoring

--- app/src/main/java/com/praktikum/bomoapp/activities/MainActivity.kt
+++ app/src/main/java/com/praktikum/bomoapp/activities/MainActivity.kt
@@ -1,40 +1,26 @@
 package com.praktikum.bomoapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-<<<<<<< HEAD
-=======
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
->>>>>>> GUI
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-<<<<<<< HEAD
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.praktikum.bomoapp.activities.MapActivity
-import com.praktikum.bomoapp.activities.SettingsActivity
-=======
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-
-
-
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.runtime.remember
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
@@ -42,41 +28,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Switch
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Divider
-
->>>>>>> GUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-<<<<<<< HEAD
-=======
-            /*
->>>>>>> GUI
-            val intentSettings = Intent(this, SettingsActivity::class.java)
-            val intentMap = Intent(this, MapActivity::class.java)
-
-            Column {
-                Button(onClick = { startActivity(intentMap) }) {
-                    Text(text = "Zeige Map")
-                }
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Button(onClick = { startActivity(intentSettings) }) {
-                    Text(text = "Zeige Settings")
-<<<<<<< HEAD
-=======
-
-                }
-            }
-            */
             AppContent()
         }
     }
@@ -283,7 +242,6 @@ class MainActivity : ComponentActivity() {
                 )
                 {
                     GridContent()
->>>>>>> GUI
                 }
             }
         }
