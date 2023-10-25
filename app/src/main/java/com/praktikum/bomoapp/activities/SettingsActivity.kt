d43203e7416c817fa1d80925cf601941c8a00629
Philipp Sellmann <philippsellmann1@gmail.com>
2023-10-25 12:53:46 +0200

    Updated UI

--- app/src/main/java/com/praktikum/bomoapp/activities/SettingsActivity.kt
+++ app/src/main/java/com/praktikum/bomoapp/activities/SettingsActivity.kt
@@ -7,8 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,33 +27,67 @@ import com.praktikum.bomoapp.viewmodels.NetworkTrackingViewModel
 
 @Composable
 fun Settings() {
+    val tabOptions = listOf("Positionierung", "Sensorik", "Speichern") // Füge hier die gewünschten Tab-Optionen hinzu
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
     val networkViewModel = NetworkTrackingViewModel(LocalContext.current)
     val gpsViewModel = GpsTrackingViewModel(LocalContext.current)
     val accViewModel = AccelerometerViewModel(LocalContext.current)
     val gyrViewModel = GyroscopeViewModel(LocalContext.current)
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column {
-            NetworkTracking(networkViewModel)
-            Spacer(modifier = Modifier.height(20.dp))
-            GpsTracking(gpsViewModel)
-            Spacer(modifier = Modifier.height(20.dp))
-            Accelerometer(accViewModel)
-            Spacer(modifier = Modifier.height(20.dp))
-            Gyroscope(gyrViewModel)
-            Spacer(modifier = Modifier.height(20.dp))
-            Compass()
-            Spacer(modifier = Modifier.height(20.dp))
-            saveDataButton()
+    TabRow(selectedTabIndex) {
+        tabOptions.forEachIndexed { index, title ->
+            Tab(
+                selected = selectedTabIndex == index,
+                onClick = { selectedTabIndex = index },
+                text = { Text(text = title) }
+            )
+        }
+    }
+    when (selectedTabIndex) {
+        0 -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
+                    NetworkTracking(networkViewModel)
+                    Spacer(modifier = Modifier.height(20.dp))
+                    GpsTracking(gpsViewModel)
+                }
+            }
+        }
+        1 -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
+                    Accelerometer(accViewModel)
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Gyroscope(gyrViewModel)
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Compass()
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+            }
+        }
+        2 -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
+                    SaveDataButton()
+                }
+            }
         }
     }
 }
 
+
 @Composable
-fun saveDataButton() {
+fun SaveDataButton() {
     Button(onClick = { DataSaver.saveAllDataServer()}) {
         Text(text = "Daten auf Server Speichern")
     }
