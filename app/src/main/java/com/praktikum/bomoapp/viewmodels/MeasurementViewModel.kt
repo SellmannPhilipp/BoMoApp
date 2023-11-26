e3b159a0fdf05e7b840ca5444557c6bf7afb2105
Philipp Sellmann <philippsellmann1@gmail.com>
2023-11-26 13:06:52 +0100

    Added timestamps to measurement

--- app/src/main/java/com/praktikum/bomoapp/viewmodels/MeasurementViewModel.kt
+++ app/src/main/java/com/praktikum/bomoapp/viewmodels/MeasurementViewModel.kt
@@ -1,14 +1,25 @@
 package com.praktikum.bomoapp.viewmodels
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MeasurementViewModel(context: Context) : ViewModel() {
+    private var startTime: Long = 0
+    private var endtime: Long = 0
     private var ctx = context
     var measurement by mutableStateOf(false)
+
+    fun start() {
+        this.startTime = System.currentTimeMillis()
+    }
+
+    fun stop() {
+        this.endtime = System.currentTimeMillis()
+    }
 
     fun toggleMeasurement() {
         measurement = !measurement
@@ -24,5 +35,11 @@ class MeasurementViewModel(context: Context) : ViewModel() {
 
         // Änderungen speichern
         editor.apply()
+
+        if(measurement) {
+            start()
+        } else {
+            stop()
+        }
     }
 }
