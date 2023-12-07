86a46178063b02f69e3639839e41116de5da2b95
LeonVolmary <leon.v2000@gmx.de>
2023-12-07 17:53:54 +0100

    Routen hinzugefuegt

--- app/src/main/java/com/praktikum/bomoapp/viewmodels/RouteViewModel.kt
+++ app/src/main/java/com/praktikum/bomoapp/viewmodels/RouteViewModel.kt
@@ -1,7 +1,5 @@
 package com.praktikum.bomoapp.viewmodels
 
-import android.util.Log
-import com.praktikum.bomoapp.MeasuringPoint
 import org.osmdroid.util.GeoPoint
 
 class RouteViewModel {
@@ -10,15 +8,31 @@ class RouteViewModel {
 
         //Route 1
         val polylinePointsOne = listOf(
-            GeoPoint(51.48122, 7.22009),
-            GeoPoint(51.48435, 7.23187),
-            GeoPoint(51.49931, 7.19633),
+            GeoPoint(51.44719, 7.27218),
+            GeoPoint(51.44688, 7.27248),
+            GeoPoint(51.44659, 7.27273),
+            GeoPoint(51.44723, 7.27354),
+            GeoPoint(51.44676, 7.27326),
+            GeoPoint(51.44753, 7.27329),
+            GeoPoint(51.4469, 7.27368),
+            GeoPoint(51.44736, 7.27277),
+            GeoPoint(51.44699, 7.2736),
+            GeoPoint(51.4471, 7.2735),
         )
 
         //Route 2
         val polylinePointsTwo = listOf(
-            GeoPoint(51.48122, 7.22009),
-            GeoPoint(51.48435, 7.23187),
+            GeoPoint(51.44798, 7.27083),
+            GeoPoint(51.44818, 7.27137),
+            GeoPoint(51.44829, 7.27171),
+            GeoPoint(51.4481, 7.27186),
+            GeoPoint(51.44794, 7.27201),
+            GeoPoint(51.44789, 7.27198),
+            GeoPoint(51.44772, 7.27213),
+            GeoPoint(51.44752, 7.27229),
+            GeoPoint(51.44728, 7.27253),
+            GeoPoint(51.44718, 7.2722),
+            GeoPoint(51.44707, 7.27178),
         )
 
         fun setSelectedRoute(value: Int) {
