87be647ab946b84443c89999ae6c86a7dbd4b166
Philipp Sellmann <philippsellmann1@gmail.com>
2024-01-09 12:11:44 +0100

    removed kotlin files from this branch

--- app/src/main/java/com/praktikum/bomoapp/viewmodels/SamplingRateViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.praktikum.bomoapp.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class SamplingRateViewModel: ViewModel() {
-    companion object {
-        var samplingRate: Int = 0
-    }
-}
